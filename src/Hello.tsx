f91b783c27393d6b519fcfd283183944abf57dad
linhnguyen <linh.nguyenthikhanh02@gmail.com>
2022-12-08 11:47:44 +0700

    reset

---
+++ src/Hello.tsx
@@ -0,0 +1,28 @@
+import { Text, View } from "react-native"
+
+export const Hello = () => {
+    return(
+        <View>
+            <Text>Khanh Linh xin chao cac ban yeu!</Text>
+            <Text>
+            Biết cách init project under git.
+- Biết cách config account git
+- Clone được project
+- Sử dụng được SSH keys
+- Dùng được những git commands cơ bản sau đây:
+  - git add
+ - git commit 
+- git branch
+- git checkout 
+- git merge
+- git rebase 
+- git pull/git push 
+- git reset
+- git revert
+- git status
+- git stash 
+- git log
+            </Text>
+        </View>
+    )
+}
