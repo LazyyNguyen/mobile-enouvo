b3ae731014310ae23f801da9fc23918dc8916812
linhnguyen <linh.nguyenthikhanh02@gmail.com>
2022-12-08 09:47:35 +0700

    add file hello

---
+++ src/Hello.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+export const Hello = () => {
+    return(
+        <View>
+            <Text>Khanh Linh xin chao cac ban yeu!</Text>
+        </View>
+    )
+}
